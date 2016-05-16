d9885c6aaeda49152d8db59e3ae6c1fc4d5fd861
fengwen1118 <fengwen1118@163.com>
2016-05-16 18:31:55 +0800

    update

--- quote-core/src/main/java/com/ihandy/quote_core/bean/other/CarInfoResponse.java
+++ quote-core/src/main/java/com/ihandy/quote_core/bean/other/CarInfoResponse.java
@@ -1,17 +1,16 @@
 package com.ihandy.quote_core.bean.other;
 
-import java.security.Timestamp;
 
 /**
  * Created by fengwen on 2016/4/29.
  */
-public class CarInfoResponse extends  BaseResponse{
+public class CarInfoResponse{
 
     private String CarUsedType; //使用性质
    
-    private Timestamp ForceEndDate;//交强险到期日期
+    private String ForceExpireDate;//交强险到期日期
 
-    private Timestamp BizEndDate;//商业险到期日期
+    private String BusinessExpireDate;//商业险到期日期
    
     private String CarVin;//车架号
    
@@ -21,9 +20,9 @@ public class CarInfoResponse extends  BaseResponse{
     
     private String LicenseOwner;// 车主姓名
 
-    private String LicenseOwnerIdCardNo;//证件号码
+    private String CredentislasNum;//证件号码
    
-    private int LicenseOwnerIdCardType;//证件类型
+    private String IdType;//证件类型
     
     private String InsuredName; //被保险人
 
@@ -33,19 +32,159 @@ public class CarInfoResponse extends  BaseResponse{
 
     private double PurchasePrice; //购买价格
 
-    private Timestamp CarRegisterDate;//车辆注册日期
+    private String CarRegisterDate;//车辆注册日期
   
     private int CarSeated;//座位数量
 
     private int CityCode ; //城市编码
-    
-    private int CompanyId; //sourceId公司
+
 
     private RelaPeopleResponse relaPeopleResponse;//相关人员
 
-    private QuoteResponse quoteResponse;//报价bean
-
-    private ClaimResponse claimResponse;//出险bean
+    private SaveQuoteResponse saveQuoteResponse;//报价bean
 
 
+    public String getCarUsedType() {
+        return CarUsedType;
+    }
+
+    public void setCarUsedType(String carUsedType) {
+        CarUsedType = carUsedType;
+    }
+
+    public String getForceExpireDate() {
+        return ForceExpireDate;
+    }
+
+    public void setForceExpireDate(String forceExpireDate) {
+        ForceExpireDate = forceExpireDate;
+    }
+
+    public String getBusinessExpireDate() {
+        return BusinessExpireDate;
+    }
+
+    public void setBusinessExpireDate(String businessExpireDate) {
+        BusinessExpireDate = businessExpireDate;
+    }
+
+    public String getCarVin() {
+        return CarVin;
+    }
+
+    public void setCarVin(String carVin) {
+        CarVin = carVin;
+    }
+
+    public String getMoldName() {
+        return MoldName;
+    }
+
+    public void setMoldName(String moldName) {
+        MoldName = moldName;
+    }
+
+    public String getEngineNo() {
+        return EngineNo;
+    }
+
+    public void setEngineNo(String engineNo) {
+        EngineNo = engineNo;
+    }
+
+    public String getLicenseOwner() {
+        return LicenseOwner;
+    }
+
+    public void setLicenseOwner(String licenseOwner) {
+        LicenseOwner = licenseOwner;
+    }
+
+    public String getCredentislasNum() {
+        return CredentislasNum;
+    }
+
+    public void setCredentislasNum(String credentislasNum) {
+        CredentislasNum = credentislasNum;
+    }
+
+    public String getIdType() {
+        return IdType;
+    }
+
+    public void setIdType(String idType) {
+        IdType = idType;
+    }
+
+    public String getInsuredName() {
+        return InsuredName;
+    }
+
+    public void setInsuredName(String insuredName) {
+        InsuredName = insuredName;
+    }
+
+    public String getPostedName() {
+        return PostedName;
+    }
+
+    public void setPostedName(String postedName) {
+        PostedName = postedName;
+    }
+
+    public String getLicenseNo() {
+        return LicenseNo;
+    }
+
+    public void setLicenseNo(String licenseNo) {
+        LicenseNo = licenseNo;
+    }
+
+    public double getPurchasePrice() {
+        return PurchasePrice;
+    }
+
+    public void setPurchasePrice(double purchasePrice) {
+        PurchasePrice = purchasePrice;
+    }
+
+    public String getCarRegisterDate() {
+        return CarRegisterDate;
+    }
+
+    public void setCarRegisterDate(String carRegisterDate) {
+        CarRegisterDate = carRegisterDate;
+    }
+
+    public int getCarSeated() {
+        return CarSeated;
+    }
+
+    public void setCarSeated(int carSeated) {
+        CarSeated = carSeated;
+    }
+
+    public int getCityCode() {
+        return CityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        CityCode = cityCode;
+    }
+
+    public RelaPeopleResponse getRelaPeopleResponse() {
+        return relaPeopleResponse;
+    }
+
+    public void setRelaPeopleResponse(RelaPeopleResponse relaPeopleResponse) {
+        this.relaPeopleResponse = relaPeopleResponse;
+    }
+
+    public SaveQuoteResponse getSaveQuoteResponse() {
+        return saveQuoteResponse;
+    }
+
+    public void setSaveQuoteResponse(SaveQuoteResponse saveQuoteResponse) {
+        this.saveQuoteResponse = saveQuoteResponse;
+    }
 }
